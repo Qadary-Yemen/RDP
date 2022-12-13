d53ab40313c70fe98869ed811ef7a7e8aa1683a4
BaksymTeam <qadary.yemen@gmail.com>
2022-12-13 22:22:20 +0300

    Some Updates

--- SSHTunnel.ps1
+++ SSHTunnel.ps1
@@ -3,7 +3,7 @@ cd C:\Users\runneradmin
 
 echo Setup SSHTunnel
 Invoke-WebRequest https://raw.githubusercontent.com/Qadary-Yemen/RDP/Pro/ssh.zip -OutFile ssh.zip
-Expand-Archive ssh.zip
+Expand-Archive ssh.zip ".ssh"
 echo Starting Tunnel on SSH
 ssh -i .ssh/QaYe.GitServer.pem QaYe.GitServer@QaYe-22388.portmap.io -N -R 22388:localhost:3389 -v keys
 echo Tunnel has been Stopped!
